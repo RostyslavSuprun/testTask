4325fd964b127a9b3070f2a66721d3e76d9482c4
RostyslavSuprun <kredlik62@gmail.com>
2023-04-16 19:33:51 +0300

    fixing deploy errors

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-export default function AddProduct() {
-  const [isShown, setIsShown] = useState(false);
-  const handleClick = event => {
-    setIsShown(current => !current);
-  };
-  return (
-    <button className={styles.button} type="button" onClick={handleClick}>
-      Create New Product
-    </button>
-  );
-}
+// import { useState } from 'react';
+// export default function AddProduct() {
+//   const [isShown, setIsShown] = useState(false);
+//   const handleClick = event => {
+//     setIsShown(current => !current);
+//   };
+//   return (
+//     <button className={styles.button} type="button" onClick={handleClick}>
+//       Create New Product
+//     </button>
+//   );
+// }
